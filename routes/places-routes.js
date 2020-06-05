f3093dad75a74abe51b576e182dce11f1188e0c2
Wanlin-Lu <luwanlin@126.com>
2020-06-05 16:33:01 +0800

    places-routes && GET test in browser

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,9 +1,45 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log("GET request in places")
-  res.status(200).json({ message: 'It works.' })
+const M_P = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u2",
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid
+  const place = M_P.filter(p => { return p.id === placeId })
+  res.json({ place })
+})
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid
+  const place = M_P.filter(p => { return p.creator === userId })
+  res.json({ place })
 })
 
 module.exports = router
