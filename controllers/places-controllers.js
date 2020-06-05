f013f9195eb835df8dfdffd1a9dd13e81736065b
Wanlin-Lu <luwanlin@126.com>
2020-06-05 17:37:07 +0800

    add place-controllers

---
+++ controllers/places-controllers.js
@@ -0,0 +1,55 @@
+const HttpError = require("../models/http-error");
+
+const M_P = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lng: 116.410829,
+      lat: 39.881913,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = M_P.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+  res.json({ place });
+}
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = M_P.find((p) => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided userId.", 404);
+  }
+  res.json({ place });
+}
+
+exports.getPlaceById = getPlaceById
+exports.getPlaceByUserId = getPlaceByUserId
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,52 +1,10 @@
 const express = require('express')
+const placeControllers = require('../controllers/places-controllers')
+
 const router = express.Router()
-const HttpError = require('../models/http-error')
 
-const M_P = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lng: 116.410829,
-      lat: 39.881913,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Emp. State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lng: 116.410829,
-      lat: 39.881913,
-    },
-    creator: "u2",
-  },
-];
+router.get('/:pid', placeControllers.getPlaceById)
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid
-  const place = M_P.find(p => { return p.id === placeId })
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-  res.json({ place })
-})
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid
-  const place = M_P.find(p => { return p.creator === userId })
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided userId.", 404)
-  }
-  res.json({ place })
-})
+router.get('/user/:uid', placeControllers.getPlaceByUserId)
 
 module.exports = router
