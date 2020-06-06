a99fbc755e7ae0399771707cab53b39e9402bdac
Wanlin-Lu <luwanlin@126.com>
2020-06-06 11:45:41 +0800

    upgrade: getPlacesByUserId

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -41,15 +41,15 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = M_P.find((p) => {
+  const places = M_P.filter(p => {
     return p.creator === userId;
   });
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided userId.", 404);
+  if (!places) {
+    throw new HttpError("Could not find places for the provided userId.", 404);
   }
-  res.json({ place });
+  res.json({ places });
 }
 
 const createPlace = (req, res, next) => {
@@ -86,7 +86,7 @@ const deletePlace = (req, res, next) => {
 }
 
 exports.getPlaceById = getPlaceById
-exports.getPlaceByUserId = getPlaceByUserId
+exports.getPlacesByUserId = getPlacesByUserId
 exports.createPlace = createPlace
 exports.updatePlace = updatePlace
 exports.deletePlace = deletePlace
