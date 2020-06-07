de3dd32af55109265cd2cc65924a174543af0b95
Wanlin-Lu <luwanlin@126.com>
2020-06-07 09:59:40 +0800

    Users -> UsersList -> UserItem

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
+const USERS = [
+  {
+    id: "u1",
+    name: "Zhao Jianye",
+    image:
+      'https://i1.hdslb.com/bfs/face/32c29e460aaaee403b38296bd492c8ee8e025590.jpg',
+    places: 3
+  },
+];
+
 const Users = () => {
   return (
-    <h1>Users</h1>
+    <UsersList items={USERS} />
   )
 }
 
