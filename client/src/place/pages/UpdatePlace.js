de180eba80cf7d43df61fc873e455b21b2506d5a
Wanlin-Lu <luwanlin@126.com>
2020-06-11 22:38:42 +0800

    form-hook: setFormData

--- client/src/place/pages/UpdatePlace.js
+++ client/src/place/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import Input from '../../shared/components/FormElements/Input'
@@ -40,24 +40,42 @@ const M_P = [
 ];
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const placeId = useParams().placeId
 
-  const identifiedPlace = M_P.find(p => p.id === placeId)
-
-  const [formState, inputHandler] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: identifiedPlace.title,
-        isValid: true
+        value: '',
+        isValid: false
       },
       description: {
-        value: identifiedPlace.description,
-        isValid: true
+        value: '',
+        isValid: false
       }
     },
-    true
-  )
+    false
+    )
+    
+  const identifiedPlace = M_P.find(p => p.id === placeId)
 
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true,
+        },
+      },
+      true
+    );
+    setIsLoading(false)
+  }, [setFormData, identifiedPlace])
+    
   const placeUpdateSubmitHandler = event => {
     event.preventDefault()
     console.log(formState.inputs)
@@ -67,6 +85,14 @@ const UpdatePlace = () => {
     return (
       <div className="center">
         <h2>Could not find place!</h2>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
       </div>
     )
   }
