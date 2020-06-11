d2b3ce8aea5eeecc4de2227c970d83206be88256
Wanlin-Lu <luwanlin@126.com>
2020-06-11 14:27:44 +0800

    Sharing Input Values && Adding Multiple Inputs

--- client/src/shared/components/FormElements/Input.js
+++ client/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 
 import { validate } from '../../util/validators'
 import "./Input.css";
@@ -27,6 +27,13 @@ const Input = props => {
     isTouched: false,
     isValid: false
   })
+
+  const { id, onInput } = props
+  const { value, isValid } = inputState
+
+  useEffect(() => {
+    props.onInput(props.id, inputState.value, inputState.isValid)
+  }, [id, value, isValid, onInput])
 
   const changeHandler = event => {
     dispatch({ type: 'CHANGE', val: event.target.value, validators: props.validators })
