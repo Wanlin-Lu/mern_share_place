c580d7b0de30bb68a08c226f26a1f0b7bc2af1b6
Wanlin-Lu <luwanlin@126.com>
2020-06-12 10:44:35 +0800

    AuthContext control UI

--- client/src/shared/components/Navigation/NavLinks.js
+++ client/src/shared/components/Navigation/NavLinks.js
@@ -29,6 +29,11 @@ const NavLinks = props => {
           <NavLink to="/auth">AUTHENTICATION</NavLink>
         </li>
       )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   );
 }
