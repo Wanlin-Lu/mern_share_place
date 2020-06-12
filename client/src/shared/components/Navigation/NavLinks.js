4a8035c81177007f4ce2e465af5d15c3774d3587
Wanlin-Lu <luwanlin@126.com>
2020-06-12 10:28:32 +0800

    AuthContext && condition Nav

--- client/src/shared/components/Navigation/NavLinks.js
+++ client/src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 
+import { AuthContext } from '../../context/auth-context'
 import './NavLinks.css'
 
 const NavLinks = props => {
+  const auth = useContext(AuthContext)
+
   return (
     <ul className="nav-links">
       <li>
@@ -11,21 +14,21 @@ const NavLinks = props => {
           All USER
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">
-          MY PLACES
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">
-          ADD PLACE
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">
-          AUTHENTICATION
-        </NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATION</NavLink>
+        </li>
+      )}
     </ul>
   );
 }
